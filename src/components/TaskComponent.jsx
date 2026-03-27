84ce78d76d3fec74b3726684223a57a16c82e6e3
gitjnichols3 <j_nichols2@mail.fhsu.edu>
2026-03-27 09:51:11 -0700

    Task 3 with comments

--- src/components/TaskComponent.jsx
+++ src/components/TaskComponent.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react';
 
 function TaskComponent(props){
 
-
+/* Set up state to store the input. */
 const [input, setInput] = useState("");
 
+/* Set up state to check if sorting is active */
 const [sort, setSort] = useState(false);
 
+/*  Filters task array sent as props from App based on input. Saves copy to leave original untouched */
 const filteredTasks = props.tasks.filter(task =>
   task.name.toLowerCase().includes(input.toLowerCase())
 );
 
+/* If sort state is true, uses copy of filteredTasks and alphabetizes by task name, otherwise uses unsorted filteredTasks */
 const displayedTasks = sort
   ? [...filteredTasks].sort((a, b) => a.name.localeCompare(b.name))
   : filteredTasks;
@@ -18,15 +21,18 @@ const displayedTasks = sort
       return (
         <>
         <h3>Search:  
+          {/* Creates input form field and updates input state if changed */}
           <input type="text" placeholder="Search tasks..." value={input} onChange={e => setInput(e.target.value)}></input>
         </h3>
         <ul>
+          {/* Maps over displayedTasks and displays as list items */}
           {displayedTasks.map(task => (
             <li key={task.id}>
               {task.name}
             </li>
           ))}
         </ul>
+        {/* Button toggles sort state. Also changes button label to indicate whether sorting is active */}
          <button onClick={() => setSort(!sort)}>{sort ? "Show Original Order" : "Sort by Name"}</button>
         </>
   );
